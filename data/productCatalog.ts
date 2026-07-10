0bf86f6c9768af0d864ac49ea9bed7dfe5c0968d
BryanFinnigan <Finnigan.Bryanm@gmail.com>
2026-07-10 12:31:03 -0400

    Include France products in combined catalog

--- data/productCatalog.ts
+++ data/productCatalog.ts
@@ -1,6 +1,7 @@
 import ukProducts from "./products.json";
 import italyProducts from "./products-italy.json";
+import franceProducts from "./products-france.json";
 
-const products = [...ukProducts, ...italyProducts];
+const products = [...ukProducts, ...italyProducts, ...franceProducts];
 
 export default products;
