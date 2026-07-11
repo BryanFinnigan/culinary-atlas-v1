90d59e90984b6b3b466c1a32f8e0abef8dc268f6
BryanFinnigan <Finnigan.Bryanm@gmail.com>
2026-07-11 10:45:08 -0400

    Include Mexico products in the catalog

--- data/productCatalog.ts
+++ data/productCatalog.ts
@@ -1,7 +1,13 @@
 import ukProducts from "./products.json";
 import italyProducts from "./products-italy.json";
 import franceProducts from "./products-france.json";
+import mexicoProducts from "./products-mexico.json";
 
-const products = [...ukProducts, ...italyProducts, ...franceProducts];
+const products = [
+  ...ukProducts,
+  ...italyProducts,
+  ...franceProducts,
+  ...mexicoProducts,
+];
 
 export default products;
